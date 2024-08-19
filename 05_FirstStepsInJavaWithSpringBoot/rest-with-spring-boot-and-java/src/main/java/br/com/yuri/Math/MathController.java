53eb1c8c743effab92af438bf2ba4efbe39d1c0a
YuriJesusVedovate <Yurivedovate@hotmail.com>
2024-08-19 00:18:37 -0300

    Criando metod para converter numeros, e tratativa de erro

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/yuri/Math/MathController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/yuri/Math/MathController.java
@@ -2,7 +2,6 @@ package br.com.yuri.Math;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,11 +10,16 @@ public class MathController {
     @GetMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(@PathVariable String numberOne, @PathVariable String numberTwo) {
         try {
-            Double num1 = Double.parseDouble(numberOne);
-            Double num2 = Double.parseDouble(numberTwo);
+            Double num1 = convertToDouble(numberOne);
+            Double num2 = convertToDouble(numberTwo);
             return num1 + num2;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Invalid input: both parameters should be valid numbers.");
+            throw new IllegalArgumentException(String.format("Invalid input: both parameters should be valid numbers. Error: %s", e.getMessage()));
         }
+    }
+
+    private Double convertToDouble(String strNumber) {
+        String number = strNumber.replaceAll(",", ".");
+        return Double.parseDouble(number);
     }
 }
