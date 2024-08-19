210264e34960c70e210a40f355a7ea8e70f0ebc4
YuriJesusVedovate <Yurivedovate@hotmail.com>
2024-08-19 01:11:29 -0300

    Ajustando status code correto

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/yuri/Math/MathController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/yuri/Math/MathController.java
@@ -1,5 +1,6 @@
 package br.com.yuri.Math;
 
+import br.com.yuri.exceptions.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,18 +9,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class MathController {
 
     @GetMapping("/sum/{numberOne}/{numberTwo}")
-    public Double sum(@PathVariable String numberOne, @PathVariable String numberTwo) {
+    public Double sum(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
         try {
             Double num1 = convertToDouble(numberOne);
             Double num2 = convertToDouble(numberTwo);
             return num1 + num2;
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(String.format("Invalid input: both parameters should be valid numbers. Error: %s", e.getMessage()));
+        } catch (IllegalArgumentException e) {
+            throw new UnsupportedMathOperationException(String.format("Invalid input: both parameters should be valid numbers. Error: %s", e.getMessage()));
+        }
+        catch (Exception e) {
+            throw new Exception("An error occurred while trying to sum the numbers. Error: " + e.getMessage());
         }
     }
 
     private Double convertToDouble(String strNumber) {
-        String number = strNumber.replaceAll(",", ".");
-        return Double.parseDouble(number);
+        if (strNumber == null || !isNumeric(strNumber.replaceAll(",", "."))) {
+            throw new IllegalArgumentException();
+        }
+        return Double.parseDouble(strNumber.replaceAll(",", "."));
+    }
+
+    private boolean isNumeric(String strNumber) {
+        return strNumber != null && strNumber.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/yuri/exceptions/ExceptionResponse.java
@@ -0,0 +1,47 @@
+package br.com.yuri.exceptions;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+public class ExceptionResponse implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String message;
+    private Date timestamp = new Date();
+    private String details;
+
+    public ExceptionResponse() {
+    }
+
+    public ExceptionResponse(String message, String details) {
+        this.message = message;
+        this.details = details;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
