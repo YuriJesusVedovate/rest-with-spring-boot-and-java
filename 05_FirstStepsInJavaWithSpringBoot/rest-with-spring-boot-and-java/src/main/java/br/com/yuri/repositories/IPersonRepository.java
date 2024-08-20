046ec4fffd4c93fc232add31a54ca11ef27fc394
YuriJesusVedovate <Yurivedovate@hotmail.com>
2024-08-20 09:27:14 -0300

    configurando conexão com o banco de dados mysql por Jpa

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/yuri/repositories/IPersonRepository.java
@@ -0,0 +1,10 @@
+package br.com.yuri.repositories;
+
+import br.com.yuri.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonRepository extends JpaRepository<Person, Long> {
+    
+}
