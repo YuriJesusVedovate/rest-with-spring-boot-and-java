3ce02eac1720c96014f5df2c87ccfd9cebd727d6
YuriJesusVedovate <Yurivedovate@hotmail.com>
2024-08-22 09:22:50 -0300

    Adicionando e configurando DozerMapper

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/yuri/mapper/DozerMapper.java
@@ -0,0 +1,23 @@
+package br.com.yuri.mapper;
+
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DozerMapper {
+
+    private static final Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+
+    public static <O, D> D parseObject(O origem, Class<D> destination) {
+        return mapper.map(origem, destination);
+    }
+
+    public static <O, D> List<D> parseListObjects(List<O> origem, Class<D> destination) {
+        List<D> destinationObjects = new ArrayList<>();
+        origem.forEach(o -> destinationObjects.add(mapper.map(o, destination)));
+        return destinationObjects;
+    }
+}
