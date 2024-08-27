8d7298cb9eb7c2b5af4f685bf8cfdb267d67f3ba
YuriJesusVedovate <Yurivedovate@hotmail.com>
2024-08-26 21:43:21 -0300

    Ajustando mapper

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/yuri/mapper/Mapper.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/yuri/mapper/Mapper.java
@@ -1,5 +1,7 @@
 package br.com.yuri.mapper;
 
+import br.com.yuri.data.vo.v1.PersonVO;
+import br.com.yuri.models.Person;
 import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
@@ -7,8 +9,15 @@ import java.util.List;
 
 public class Mapper {
 
-    //private static final Mapper mapper = DozerBeanMapperBuilder.buildDefault();
     private static final ModelMapper mapper = new ModelMapper();
+
+    static {
+        mapper.createTypeMap(Person.class, PersonVO.class)
+                .addMapping(src -> src.getId(), PersonVO::setKey);
+
+        mapper.createTypeMap(PersonVO.class, Person.class)
+                .addMapping(src -> src.getKey(), Person::setId);
+    }
 
     public static <O, D> D parseObject(O origem, Class<D> destination) {
         return mapper.map(origem, destination);
