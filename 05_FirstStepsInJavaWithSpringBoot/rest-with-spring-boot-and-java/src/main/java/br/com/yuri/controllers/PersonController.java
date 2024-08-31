7a1faf273e378c172249438aae981617d72ba754
YuriJesusVedovate <Yurivedovate@hotmail.com>
2024-08-31 15:07:17 -0300

    Update PersonController.java

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/yuri/controllers/PersonController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/yuri/controllers/PersonController.java
@@ -2,6 +2,10 @@ package br.com.yuri.controllers;
 
 import br.com.yuri.data.vo.PersonVO;
 import br.com.yuri.services.person.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +15,7 @@ import static br.com.yuri.util.MediaType.*;
 
 @RestController
 @RequestMapping("/api/person")
+@Tag(name = "Person Endpoint", description = "Endpoints for Managing People")
 public class PersonController {
 
     private final PersonService service;
@@ -31,6 +36,13 @@ public class PersonController {
             value = "/all",
             produces = {APPLICATION_JSON, APPLICATION_XML, APPLICATION_YML},
             consumes = {APPLICATION_JSON, APPLICATION_XML, APPLICATION_YML})
+    @Operation(summary = "Find all people recorded in the database", description = "Find all people recorded in the database", tags = {"Person Endpoint"}, responses = {
+            @ApiResponse(responseCode = "200", description = "Success", content = {
+                    @Content(mediaType = APPLICATION_JSON)
+
+            }),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+    })
     public ResponseEntity<List<PersonVO>> all() {
         return ResponseEntity.ok(service.findAll());
     }
