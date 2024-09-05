e93143ae3f3b7c2398e71e786926d43c74810d51
YuriJesusVedovate <Yurivedovate@hotmail.com>
2024-09-05 20:40:00 -0300

    Create Book.java

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/yuri/models/Book.java
@@ -0,0 +1,80 @@
+package br.com.yuri.models;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "author", nullable = false, length = 80)
+    private String author;
+    @Column(name = "launch_date", nullable = false)
+    private Date releaseDate;
+    @Column(name = "price", nullable = false)
+    private boolean price;
+    @Column(name = "title", nullable = false, length = 80)
+    private String title;
+
+    public Book() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isPrice() {
+        return price;
+    }
+
+    public void setPrice(boolean price) {
+        this.price = price;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id && price == book.price && Objects.equals(author, book.author) && Objects.equals(releaseDate, book.releaseDate) && Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, author, releaseDate, price, title);
+    }
+}
